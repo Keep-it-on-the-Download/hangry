450e26026935f9ecab9704788860c56ff2763512
Alan Bielik <abielik92@gmail.com>
2021-03-18 15:32:15 -0400

    Chore: configured Firebase Authentication

---
+++ src/Authentication.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import firebase from './Firebase';
+import 'firebase/auth';
+
+const auth = firebase.auth();
+
+function SignIn() {
+  const loginWithGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider);
+  };
+  return <button onClick={loginWithGoogle}>Login With Google</button>;
+}
+
+function SignOut() {
+  return (
+    auth.currentUser && <button onClick={() => auth.signOut()}>Sign Out</button>
+  );
+}
+
+export { SignIn, SignOut };
