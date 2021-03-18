0992a2a0cdceba23b99f746077c124f277e4233e
Anastasha Rachel Gunawan <rachelgunawan1999@gmail.com>
2021-03-18 17:57:40 -0400

    Chore: Set up browserrouter on App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
 
 import Profile from './components/Profile';
+import { BrowserRouter, Route } from 'react-router-dom';
+import RestaurantDetails from './components/RestaurantDetails';
 
 function App() {
   return (
-    <div className='App'>
-      <Profile />
-    </div>
+    <BrowserRouter>
+      <div className='App'>
+        <Route exact path='/profile' component={Profile} />
+        <Route exact path='/restaurants/1' component={RestaurantDetails} />
+        {/* PSA: the route is temporarily /restaurants/1 so i can see it while updating*/}
+      </div>
+    </BrowserRouter>
   );
 }
 
