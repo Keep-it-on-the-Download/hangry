dc89a64a3d122ddcd2d896c24674d60aaa68c068
Charles Dell'Orto <charlie.dellorto@gmail.com>
2021-03-19 14:32:56 -0400

    FEAT: added folders for redux store and reducers

---
+++ src/reducers/user.js
@@ -0,0 +1,10 @@
+const initialState = { data: {}, isLoading: true };
+
+const user = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default user;
---
+++ src/store/index.js
@@ -0,0 +1,21 @@
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { createLogger } from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
+
+// Reducers
+import user from '../reducers/user';
+import friends from '../reducers/friends';
+
+const reducer = combineReducers({
+  user,
+  friends,
+});
+
+const middleware = composeWithDevTools(
+  applyMiddleware(thunkMiddleware, createLogger({ collapsed: true }))
+);
+
+const store = createStore(reducer, middleware);
+
+export default store;
