2c74a4eedaee9d3a2b4ea59c5096b8cb9425ebc7
Charles Dell'Orto <charlie.dellorto@gmail.com>
2021-03-19 14:22:44 -0400

    FEAT: started work on pulling data from firestore

--- src/App.js
+++ src/App.js
@@ -6,14 +6,17 @@ import RestaurantDetails from './components/RestaurantDetails';
 import MainScreen from './components/MainScreen';
 import Profile from './components/Profile';
 
+import LoginScreen from './components/LoginScreen';
+
 function App() {
   return (
     <BrowserRouter>
       <div className='App'>
+        <Route exact path='/login' component={LoginScreen} />
         <Route exact path='/profile' component={Profile} />
         <Route exact path='/restaurants/1' component={RestaurantDetails} />
         {/* PSA: the route is temporarily /restaurants/1 so i can see it while updating*/}
-        <Route exact path='/home' component={MainScreen} />
+        <Route exact path='/' component={MainScreen} />
       </div>
     </BrowserRouter>
   );
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,5 +1,9 @@
 import React from 'react';
 
+import firebase from '../Firebase';
+import 'firebase/auth';
+import 'firebase/firestore';
+
 // import statements Material-UI
 import { withStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
@@ -11,6 +15,7 @@ import ListItem from '@material-ui/core/ListItem';
 import ListItemAvatar from '@material-ui/core/ListItemAvatar';
 import ListItemText from '@material-ui/core/ListItemText';
 import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
 
 // Temp Profile Pic
 import AccountCircle from '@material-ui/icons/AccountCircle';
@@ -47,40 +52,84 @@ const styles = (theme) => ({
 class Profile extends React.Component {
   render() {
     const { classes } = this.props;
+    const user = firebase.auth().currentUser;
+
+    const db = firebase.firestore();
+    const alanFriendsRef = db
+      .collection('users')
+      .doc('abielik')
+      .collection('friends');
+
+    console.log(('ALANSFRIENDSREF', alanFriendsRef));
+
+    alanFriendsRef.get().then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        // doc.data() is never undefined for query doc snapshots
+        console.log(doc.id, ' => ', doc.data());
+
+        const friendRef = db.doc(doc.data().ref.path);
+        console.log('FriendRef', friendRef);
+
+        friendRef
+          .get()
+          .then((doc) => {
+            if (doc.exists) {
+              console.log('Document data:', doc.data());
+            } else {
+              // doc.data() will be undefined in this case
+              console.log('No such document!');
+            }
+          })
+          .catch((error) => {
+            console.log('Error getting document:', error);
+          });
+      });
+    });
+
+    // console.log('USER: ', user.displayName);
+    // console.log('IMG: ', user.photoURL);
     return (
       <Container maxWidth='sm'>
-        <Grid container className={classes.profile}>
-          <Grid item xs={6} className={classes.settings}>
-            <IconButton>
-              <Settings />
-            </IconButton>
-          </Grid>
-          <Grid item xs={6} className={classes.notifications}>
-            <IconButton>
-              <Notifications />
-            </IconButton>
-          </Grid>
-          <Grid item xs={12} className={classes.imageContainer}>
-            <Avatar alt='Name' className={classes.profileImage}>
-              <AccountCircle />
-            </Avatar>
-          </Grid>
-          <Grid item xs={12}>
-            First Last
-          </Grid>
-        </Grid>
-        <List>
-          <ListItem>Friends</ListItem>
-          <Divider variant='fullWidth' component='li' />
-          <ListItem>
-            <ListItemAvatar>
-              <Avatar>
-                <AccountCircle />
-              </Avatar>
-            </ListItemAvatar>
-            <ListItemText primary='First Last' secondary='Some info' />
-          </ListItem>
-        </List>
+        {user ? (
+          <React.Fragment>
+            <Grid container className={classes.profile}>
+              <Grid item xs={6} className={classes.settings}>
+                <IconButton>
+                  <Settings />
+                </IconButton>
+              </Grid>
+              <Grid item xs={6} className={classes.notifications}>
+                <IconButton>
+                  <Notifications />
+                </IconButton>
+              </Grid>
+              <Grid item xs={12} className={classes.imageContainer}>
+                <Avatar
+                  alt='Name'
+                  src={user.photoURL}
+                  className={classes.profileImage}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                {user.displayName}
+              </Grid>
+            </Grid>
+            <List>
+              <ListItem>Friends</ListItem>
+              <Divider variant='fullWidth' component='li' />
+              <ListItem>
+                <ListItemAvatar>
+                  <Avatar>
+                    <AccountCircle />
+                  </Avatar>
+                </ListItemAvatar>
+                <ListItemText primary='First Last' secondary='Some info' />
+              </ListItem>
+            </List>
+          </React.Fragment>
+        ) : (
+          <Typography variant='h1'>No User Found</Typography>
+        )}
       </Container>
     );
   }
