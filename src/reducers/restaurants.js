e4238f8f2d7743e19dd1f99355fc7b57175bfcf2
Charles Dell'Orto <charlie.dellorto@gmail.com>
2021-04-01 11:23:25 -0400

    FEAT: added firebase functions for managing env variables

--- src/reducers/restaurants.js
+++ src/reducers/restaurants.js
@@ -1,12 +1,14 @@
 import axios from 'axios';
 
+const functions = require('firebase-functions');
+
 let BATCH_SIZE = 10;
 let BATCH_NUM = 1;
 let STORAGE = [];
 
 const YELP_API_KEY =
   process.env.NODE_ENV === 'production'
-    ? process.env.YELP_API_KEY
+    ? functions.config().yelp.key
     : process.env.REACT_APP_YELP_API_KEY;
 
 const ADD_TO_SELECTED = 'ADD_TO_SELECTED';
@@ -43,6 +45,10 @@ export function getInitialRestaurants() {
     // Check Firestore for available shared workers
     // Query Yelp using shared parameters and correct offset
     // push next set of restaurants to FireStore
+
+    console.log('NODE ENV -->', process.env.NODE_ENV);
+    console.log('YELP API KEY -->', YELP_API_KEY);
+
     const { data } = await axios.get(
       `${'https://cors.bridged.cc/'}https://api.yelp.com/v3/businesses/search`,
       {
