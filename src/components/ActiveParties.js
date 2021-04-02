4a96bf50b86d4cc4d10ec45c2350cc76508081a3
Charles Dell'Orto <charlie.dellorto@gmail.com>
2021-04-02 16:38:21 -0400

    FIX removed unused imports

--- src/components/ActiveParties.js
+++ src/components/ActiveParties.js
@@ -4,7 +4,6 @@ import { withStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
 
